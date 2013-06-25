0f7dccb11f12b8a17e00205588285d29566f665b
Jeremiah <jeremiah@jeremiahpwilliams.com>
2013-06-25 01:50:41 -0400

    add lb add and list

--- build_with_pub_key.py
+++ build_with_pub_key.py
@@ -19,7 +19,9 @@ parser.add_argument('-n',metavar='#Servers', dest='num_of_servers', type=int, he
 parser.add_argument('-d', required=False, dest='data_center', type=str, help='DFW,ORD,SYD,LON', default='DFW')
 parser.add_argument('-l', required=False, action='store_true', help='List Image IDs', dest='list_images')
 parser.add_argument('-f', required=False, action='store_true', help='List Flavors', dest='list_flavs')
-parser.add_argument('-i', required=False, dest='server_id', type=str, help='Provide Image ID for Build',default='da1f0392-8c64-468f-a839-a9e56caebf07')
+parser.add_argument('-i', required=False, dest='server_id', type=str, help='Provide Image ID for Build',default='da1f0392-8c64-468f-a839-a9e56caebf07', metavar='image_id')
+parser.add_argument('-L', required=False, dest='load_bal_id', type=str, help='ID of Load Balencer to Build server behind')
+parser.add_argument('-LL', required=False, action='store_true', help='List Load Balencers and IDs', dest='list_load_balencers')
 args = parser.parse_args()
 #Auth Section
 pyrax.set_setting("identity_type", "rackspace")
@@ -57,11 +59,27 @@ def flav_lister():
 		print "vCPUs: ",flv.vcpus
 		print
 	sys.exit()
+def list_load_balencers_id():
+	clb = pyrax.cloud_loadbalancers
+	try: 
+		clb_list = clb.list()
+	except ne.EndpointNotFound:
+		print "Wrong Account Type For The DC You Selected"
+		sys.exit()
+	print "**Load Balencer List**"
+	for i in clb_list:
+		print 'Name:',i.name
+		print 'ID:',i.id
+		print 'Port:',i.port
+		print
+	sys.exit()
+
 if args.list_images is True:
 	image_check()
 if args.list_flavs is True:
 	flav_lister()
-
+if args.list_load_balencers is True:
+	list_load_balencers_id()
 
 if args.local_file is not None:
 	f = open(args.local_file, 'r')
@@ -72,7 +90,23 @@ else:
 
 
 server_obj = []
+server_private_ip_list = []
 #Server Build 
+def build_behind_load_balencers(load_balancer_id, priv_ip_listing):
+	clb = pyrax.cloud_loadbalancers
+	lb = clb.list()
+	lb_listing = []
+	lb_porter = []
+	for i in lb:
+		if str(i.id) == load_balancer_id:
+			lb_listing.append(i)
+			lb_porter.append(i.port)
+	lb = lb_listing[0]
+	for servers_priv in priv_ip_listing:
+		print servers_priv
+		new_node = clb.Node(address=servers_priv, port=lb_porter[0], condition="ENABLED")
+		lb.add_nodes([new_node])
+
 def server_check(server_list):
 	for server in server_list:
 		pyrax.utils.wait_until(server, "status", ["ACTIVE","ERROR"], interval=30, attempts=0, verbose=True)
@@ -94,11 +128,16 @@ for server in server_obj:
 		curr_img = server.image
 		server.rebuild(curr_img)
 
-
 for server in server_obj:
 	if len(server.networks['public'][0]) > 15:
 		print server.networks['public'][1]
 	else:
 		print server.networks['public'][0]
+	server_private_ip_list.append(server.networks['private'][0])
 	if files is None:
 			print "Admin Password:", server.adminPass
+
+
+
+if len(args.load_bal_id) is not 0:
+	build_behind_load_balencers(args.load_bal_id, server_private_ip_list)
